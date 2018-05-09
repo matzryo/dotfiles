61c81cdffad8ba60f05723e543a7affd3b675d4b
matzryo <matzryo@gmail.com>
2018-05-09 14:56:34 +0900

    [update] 自動でtmuxを立ち上げる

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
 alias be='bundle exec'
+
+# 自動でtmuxを立ち上げる
+# https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
