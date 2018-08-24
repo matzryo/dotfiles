460104410c9a2cf7b105310cdf77b645c2bb88e1
matzryo <matzryo@gmail.com>
2018-08-24 15:39:33 +0900

    [update] cdしたらlsする

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
 alias be='bundle exec'
 
+# cdしたらlsする
+functions -c cd standard_cd
+
+function cd
+  standard_cd $argv; and ls
+end
+
 # 自動でtmuxを立ち上げる
 # https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
 if status is-interactive
