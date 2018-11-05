d3f6d8dda6d1e6adf3bf573a11214e329ef1d230
matzryo <matzryo@gmail.com>
2018-11-05 16:21:22 +0900

    [update] $HOME/.config/fish/config.fish が存在しないケースの対応

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-readonly DOTFILES=(.bashrc .bash_profile .vimrc .tmux.conf .config/fish/config.fish)
+readonly DOTFILES=(.bashrc .bash_profile .vimrc .tmux.conf)
 
 for file in ${DOTFILES[@]}
 do
   ln -si $HOME/dotfiles/$file $HOME/$file
+done
+
+readonly NESTED_DOTFILES=(.config/fish/config.fish)
+
+for file in ${NESTED_DOTFILES[@]}
+do
+  source_file=$HOME/dotfiles/$file
+  target_file=$HOME/$file
+
+  mkdir -p $(dirname $target_file) && touch $(basename $target_file)
+  ln -si $source_file $target_file
 done
 
 mkdir -p $HOME/.vim/.backup $HOME/.vim/.swp $HOME/.vim/.undo
